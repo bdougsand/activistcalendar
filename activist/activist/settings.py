e1c3ddd8c8cec3889cc94264f1db0585e9c2f587
LalSoong <nataliandrea@ug.uchile.cl>
2016-02-28 19:16:06 -0300

    modifications to startserver

--- activist/activist/settings.py
+++ activist/activist/settings.py
@@ -15,7 +15,7 @@ DATABASES = {
         'NAME': 'activist',                      # Or path to database file if using sqlite3.
         'USER': 'activist',                      # Not used with sqlite3.
         'PASSWORD': 'activist',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
