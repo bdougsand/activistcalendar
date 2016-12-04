0e5087e5b08694f7f761c3da31c738601dded470
Diego Martinez <diego001@gmail.com>
2016-12-04 17:28:05 -0500

    Let's try this now.

--- activist/activist/settings.py
+++ activist/activist/settings.py
@@ -3,7 +3,6 @@
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SITE_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 
 
 DEBUG = True
@@ -62,7 +61,8 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
